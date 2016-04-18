ab5ee4e1b0f452a893efcb11dfc7656ec956f19a
galaterro <adrix1992@gmail.com>
2016-04-18 19:14:43 +0200

    humano

--- src/es/cesalberca/populator/model/Humano.java
+++ src/es/cesalberca/populator/model/Humano.java
@@ -10,5 +10,59 @@ package es.cesalberca.populator.model;
  * @author dam
  */
 public class Humano {
+    private String name;
+    private String lName;
+    private int salary;
+    private int bYear;
+    private String profCat;
+
+    public Humano(String name, String lName, int salary, int bYear, String profCat) {
+        this.name = name;
+        this.lName = lName;
+        this.salary = salary;
+        this.bYear = bYear;
+        this.profCat = profCat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getbYear() {
+        return bYear;
+    }
+
+    public void setbYear(int bYear) {
+        this.bYear = bYear;
+    }
+
+    public String getProfCat() {
+        return profCat;
+    }
+
+    public void setProfCat(String profCat) {
+        this.profCat = profCat;
+    }
+    
     
 }
