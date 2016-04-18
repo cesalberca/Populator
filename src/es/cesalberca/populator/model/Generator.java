f920e0f01b9e7b5aed5d507ae1ad68d8d1137e3b
galaterro <adrix1992@gmail.com>
2016-04-18 19:52:57 +0200

    Static a no static

--- src/es/cesalberca/populator/model/Generator.java
+++ src/es/cesalberca/populator/model/Generator.java
@@ -12,23 +12,64 @@ import java.util.ArrayList;
  * @author dam
  */
 public class Generator {
+    private ArrayList<String> names;
     
-    public static ArrayList<Human> generate(int numHumans){
+    public ArrayList<Human> generate(int numHumans){
         ArrayList<Human> humans = humans = new ArrayList(numHumans);
-        Human humano = new Human(getRandomName(), getRandomLastName(), 
-                getRandomSalary(), getRandomBYear(), getRandomProfCat());
+        Human human = new Human(
+                getRandomName(), 
+                getRandomLastName(), 
+                getRandomSalary(), 
+                getRandomBYear(), 
+                getRandomProfCat());
         return humans;
     }
     
-    public static String getRandomName(){
-        return "c";
+    public void setNames() {
+        names = new ArrayList();
+        names.add("Aarón");
+        names.add("Abdalá");
+        names.add("Abdón");
+        names.add("Abdul");
+        names.add("Abel");
+        names.add("Abelardo");
+        names.add("Abilio");
+        names.add("Abner");
+        names.add("Abraham");
+        names.add("Absalón");
+        names.add("Acacio");
+        names.add("Accio");
+        names.add("Adalberto");
+        names.add("Adán");
+        names.add("Adelfo");
+        names.add("Adelino");
+        names.add("Adelio");
+        names.add("Adolfo");
+        names.add("Adonaí");
+        names.add("Adonis");
+        names.add("Adrián");
+        names.add("Adriano");
+        names.add("Adulfo");
+        names.add("Africano");
+        names.add("Afrodisio");
+        names.add("Agamenón");
+        names.add("Agapito");
+        names.add("Agesilao");
+        names.add("Agripino");
+        names.add("Agustín");
     }
     
-    public static String getRandomLastName(){
+    public String getRandomName(ArrayList<String> names){
+        int max = names.size();
+        int randPosition = (int)Math.random() * max;
+        return names.get(randPosition);
+    }
+    
+    public String getRandomLastName(){
         return "a";
     }
     
-    public static int getRandomSalary(){
+    public int getRandomSalary(){
         int salary_min=8000;
         int salary_max=150000;
         return (int) ((Math.random() + salary_min) * salary_max);
